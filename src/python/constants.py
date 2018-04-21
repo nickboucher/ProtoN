5d7472fa167cdc2919ca9122195068e0ef50b08e
Aron Szanto <aszanto9@gmail.com>
2018-04-21 14:10:46 -0400

    constants + start of encoder py

---
+++ src/python/constants.py
@@ -0,0 +1,20 @@
+##
+## constants.py
+## ProtoN Constant Definitions
+## April 20, 2018
+
+## This code should never be executed by itself, but rather imported as an
+## typecode reference.
+##
+PROTOCOL_VERSION = 0x1
+
+TYPE_NULL = 0x0
+TYPE_STRING = 0x1
+TYPE_INT = 0x2
+TYPE_FLOAT = 0x3
+TYPE_BOOL = 0x4
+
+TYPE_PAIR = 0x5
+
+TYPE_LIST = 0x6
+TYPE_OBJECT = 0x7
