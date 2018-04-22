3f23d1ff7bb215d0c53e3e73754449705abedc51
Aron Szanto <aszanto9@gmail.com>
2018-04-21 21:36:31 -0400

    first pass at python encoder

--- src/python/encoder.py
+++ src/python/encoder.py
@@ -4,27 +4,73 @@ from constants import *
 import struct
 
 
-class Encoder:
-    def __init__():
-        self.blist = [pack('!B', PROTOCOL_VERSION)]
+from constants import *
+import struct
 
-    def clear():
-        self.blist = [pack('!B', PROTOCOL_VERSION)]
+primitive_types = (int, str, float, type(None))
 
-    def pack_message():
-        return b''.join(self.blist)
+def pack_message(bytelist):
+    bytelist.insert(0, pack('!B', PROTOCOL_VERSION]
+    return b''.join(bytelist)
 
-    def encode_variable(data):
-        if data is None:
-            return pack('!B', TYPE_NULL)
-        if isinstance(data, str):
-            utf = data.encode('utf-8')
-            utflen = len(utf)
-            return pack('!Bh', TYPE_STRING, utflen) + utf
-        if isinstance(data, int):
-            if (2**31 + 1) < data < (2**31 - 1):
-                return pack('!B?i', TYPE_INT, False, data)
-            else:
-                return pack('!B?q', TYPE_INT, True, data)
-        if isinstance(data, float):
-            return pack('!Bd', TYPE_FLOAT, data)
+def encode_variable(data):
+    """Infers the type of data, then packs it into a network-order
+    bytestring in accordance with the wire protocol. Data must be of type:
+    int, str, float, None.
+    @param data - the data to pack"""
+    if data is None:
+        return pack('!B', TYPE_NULL)
+    if isinstance(data, str):
+        utf = data.encode('utf-8')
+        utflen = len(utf)
+        return pack('!Bh', TYPE_STRING, utflen) + utf
+    if isinstance(data, int):
+        if abs(data) < (2**31 - 1):
+            return pack('!B?i', TYPE_INT, False, data)
+        else:
+            return pack('!B?q', TYPE_INT, True, data)
+    if isinstance(data, float):
+        return pack('!Bd', TYPE_FLOAT, data)
+
+    else:
+        raise TypeError("Wire protocol does not support this data type. \
+        Expected: int, str, float, None. Got:", type(data))
+def encode_object(obj, bytelist, memo=None):
+    if memo is None:
+        memo = set()
+
+    if isinstance(obj, primitive_types):
+        bytelist.append(encode_variable(obj))
+    elif isinstance(obj, Mapping):
+        if id(obj) not in memo:
+            memo.add(id(obj))
+        else:
+            raise ValueError("ProtoN does not support circular references within objects")
+
+        bytelist.append(pack("!Bh", TYPE_OBJECT, len(obj.items())))
+
+        for key, value in obj.items():
+            assert(isinstance(key, str))
+            bytelist.append(encode_variable(key))
+            encode_object(value, bytelist, memo)
+        memo.remove(id(obj))
+    elif isinstance(obj, (Sequence, Set)) and not isinstance(obj, string_types):
+        if id(obj) not in memo:
+            memo.add(id(obj))
+        else:
+            raise ValueError("ProtoN does not support circular references within objects")
+
+        bytelist.append(pack("!Bh", TYPE_LIST, len(obj)))
+
+        for elt in obj:
+            encode_object(elt, bytelist, memo)
+        memo.remove(id(obj))
+
+    elif hasattr(obj, '__dict__'):
+        encode_object(vars(obj), bytelist)
+
+def encode(data):
+    bytelist = []
+    encode_object(data, bytelist)
+    msg = pack_message(bytelist)
+    return msg
