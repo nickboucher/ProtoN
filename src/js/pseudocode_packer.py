0198b822b98a21a3ab7f52b2a63fe89fed5d9b71
Nicholas Boucher <nboucher@college.harvard.edu>
2018-04-27 01:43:06 -0400

    Updated pseudocode. Added js protobuf ex.

--- src/js/pseudocode_packer.py
+++ src/js/pseudocode_packer.py
@@ -5,16 +5,31 @@ bytes = [0x0] * 64
 byte_idx = 0
 counter = 0
 
-# What type is bits?
-def pack_bit(bits):
-  for bit in bits:
-    if counter >= 8:
+# bytes is Uint8Array
+def pack_bit(length, bytes):
+  def incr_idx():
       if byte_idx >= len(bytes)-1:
-        bytes += [0x0] * 64
+        bytes += [0o0] * 64
       byte_idx += 1
       counter = 0
-    # What type is bit? Does this shift create the correct result?
-    bytes[counter] = bytes[counter] & (bit << counter)
+  for byte in bytes:
+    # Calculate how many bits are left to pack in this byte
+    if length <= 8:
+        remaining = length
+        length = 0
+    else:
+        remaining = 8
+        length -= 8
+
+    if counter >= 8:
+      incr_idx()
+    if counter != 0:
+        bytes[byte_idx] = bytes[byte_idx] | (byte >>> counter)
+        incr_idx()
+        bytes[byte_idx] = byte << counter
+        counter = 8 - counter
+    else:
+        bytes[byte_idx] = byte
 
 def get_bytes():
   # Currently end-pads with zeros to next byte. How to remove this?
